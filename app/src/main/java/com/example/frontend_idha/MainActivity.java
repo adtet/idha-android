502169a65114c2c8b94e1654582100b2bb8c72a1
Adiyatma Koto <adiyatma.padang@gmail.com>
2021-05-29 09:42:44 +0700

    first commit

---
+++ app/src/main/java/com/example/frontend_idha/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.frontend_idha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
